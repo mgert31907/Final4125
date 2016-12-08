80f451fd1a96135f9991fc656a8a2ef3e01cc7a1
mgert31907 <gertjegerdes_mark@columbusstate.edu>
2016-12-08 17:01:44 -0500

    Add files via upload

---
+++ Models/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Final4125.Models
+{
+    public class Test
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class TestDBcontext : DbContext
+    {
+        public DbSet<Test> Movies {get; set;}
+    }
+}
