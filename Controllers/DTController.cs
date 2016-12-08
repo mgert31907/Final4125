4a5993a1fd97f2e3fa51af9e9c64179b79de7296
mgert31907 <gertjegerdes_mark@columbusstate.edu>
2016-12-08 17:00:30 -0500

    Add files via upload

---
+++ Controllers/DTController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final4125.Controllers
+{
+    public class DTController : Controller
+    {
+        // GET: DT
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Today()
+        {
+            string datetimeNow = DateTime.Now.ToString();
+            return HttpUtility.HtmlEncode("Right now is " + datetimeNow);
+        }
+            
+    }
+}
